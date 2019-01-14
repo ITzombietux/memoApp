85fb04ba6db2ca47491bfd4928cf2eaa77ba315d
Mac <zombietux@macui-macbook-pro-2.local>
2019-01-14 15:31:55 +0900

    Initial Commit

---
+++ MemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by zombietux on 14/01/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
